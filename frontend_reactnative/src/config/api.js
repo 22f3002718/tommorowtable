261016b42136d88c495b59f016b48b7c9daeea03
Your Name <you@example.com>
2025-10-17 09:24:41 +0530

    added react_native

--- frontend_reactnative/src/config/api.js
+++ frontend_reactnative/src/config/api.js
@@ -4,7 +4,7 @@
 // Example: export const API_URL = 'http://192.168.1.100:8001/api';
 // For production: use your deployed backend URL
 
-export const API_URL = 'http://localhost:8001/api';
+export const API_URL = 'http://192.168.29.8:8001/api';
 
 // Google Maps API Key
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyC639Pry6U9QIxYu-RJzl_dzsSshyew6Ks';
