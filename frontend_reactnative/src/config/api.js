d831f866952151f9306c098908a1d3250a11d643
Your Name <you@example.com>
2025-10-17 23:02:11 +0530

    sdk 54

--- frontend_reactnative/src/config/api.js
+++ frontend_reactnative/src/config/api.js
@@ -4,7 +4,8 @@
 // Example: export const API_URL = 'http://192.168.1.100:8001/api';
 // For production: use your deployed backend URL
 
-export const API_URL = 'http://localhost:8001/api';
+export const API_URL = 'https://b623952c399a.ngrok-free.app/api';
+
 
 // Google Maps API Key
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyC639Pry6U9QIxYu-RJzl_dzsSshyew6Ks';
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyA1d7_ceSCSsnyrlVfJpl5VZQcK3XTs9wc';
